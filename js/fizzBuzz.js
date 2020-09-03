9865b1ddb34a46751e4f61d423f1256d2b0d9b80
donelleharris <donelle.harris@gmail.com>
2020-09-03 09:18:55 -0500

    morning push ups & mapbox lesson

--- js/fizzBuzz.js
+++ js/fizzBuzz.js
@@ -1,10 +1,17 @@
-for (let i = 1; i <= 100; i++){
-    if (i % 3 === 0 && i % 5 === 0){
-        console.log(i + " fizz, buzz")
-    } else if (i % 3 === 0) {
-        console.log(i + " fizz")
-    } else if (i % 5 === 0) {
-        console.log(i + " buzz")
-    } else {
-        console.log(i)}
+function fizzBuzz(num){
+    for (let i = 1; i <=num; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
 }
+fizzBuzz(150);
+
+var i = 1
+while ()
---
+++ js/quiz.js
@@ -0,0 +1,7 @@
+function check(i) {
+    while (i > 10) {
+        console.log(i);
+        i++
+    }
+}
+    check(0);
