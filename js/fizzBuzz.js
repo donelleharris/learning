47510bb3d5a74bc2a3c06014cd17fd96b3a5ff65
Donelle Harris <donelle.harris@gmail.com>
2020-09-17 11:08:29 -0500

    morning pushups/warmups

--- js/fizzBuzz.js
+++ js/fizzBuzz.js
@@ -12,6 +12,22 @@ function fizzBuzz(num){
     }
 }
 fizzBuzz(150);
+(function (){
+    $(document).ready(function (){
+        $('#goFizzBuzz').click(function (){
+            var num = $('#input').val();
+            for (let i = 1; i <=num; i++) {
+                if (i % 15 === 0) {
+                    $('#fizzbuzz').append('<p>fizzbuzz</p>')
+                } else if (i % 3 === 0) {
+                    $('#fizzbuzz').append('<p>fizz</p>')
+                } else if (i % 5 === 0) {
+                    $('#fizzbuzz').append('<p>buzz</p>')
+                } else {
+                    $('#fizzbuzz').append('<p>' + i + '</p>')
+                }
+            }
+        });
 
-var i = 1
-while ()
+    });
+})();
