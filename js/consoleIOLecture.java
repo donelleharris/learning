5549a0c8546b80386aacff3989a77132f4eb49c8
Donelle Harris <donelle.harris@gmail.com>
2020-10-04 19:41:32 -0500

    lecture notes/files

---
+++ js/consoleIOLecture.java
@@ -0,0 +1,14 @@
+
+public class consoleIOLecture {
+    public static void main(String[] args) {
+        System.out.println("Console IO Lecture");
+
+        System.out.print("here");
+        System.out.print("there");
+
+        System.out.print("here\n");
+        System.out.print("there\n");
+
+        System.out.println("Hello" + " " + "World!");
+    }
+}
--- js/promises-lecture.js
+++ js/promises-lecture.js
@@ -1,3 +1,28 @@
+// Promises: a tol for handling asynchronous events in JavaScript. A promise represents an event that might not yet have happened.
+// A promise will be in one of three states:
+        // pending: the event has not yet happened
+        // resolved: the event has happened successfully
+        // rejected: the event has happened and an error condition occurred
+// Fetch: alternative to using jQuery to make AJAX requests
+// Fetch will accept a URL and return a 'promise' representing the AJAX request.
+
+// const myPromise = fetch('https://api.gihub.com/users');
+//     myPromise.then(response => console.log(response));
+//     myPromise.catch(error => console.log(error));
+
+const githubPromise = fetch('https://api.github.com/repositories');
+const bitbucketPromise = fetch('https://api.bitbucket.org/2.0/repositories');
+
+Promise.all([githubPromise, bitbucketPromise])
+    .then(function(data) {
+        // here data is an array of the resolved values from each promise
+        // we can now do something with both pieces of data
+    })
+    .catch(function(error) {
+        // handle errors
+    });
+
+
 // let redTurtle = Math.floor(Math.random()*10000)
 // let purpleTurtle = Math.floor(Math.random()*10000)
 // let blueTurtle = Math.floor(Math.random()*10000)
@@ -34,13 +59,13 @@
 //         })
 //     })
 // });
-Promise.resolve('one')
-.then((one) => {
-    console.log(one);
-    return'two';
-}).then((two) => {
-    console.log(two);
-    return 'three';
-}).then((three) => {
-    console.log(three);
-});
+// Promise.resolve('one')
+// .then((one) => {
+//     console.log(one);
+//     return'two';
+// }).then((two) => {
+//     console.log(two);
+//     return 'three';
+// }).then((three) => {
+//     console.log(three);
+// });
